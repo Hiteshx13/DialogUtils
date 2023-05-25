9dd826d60c4e71110efe96faa2350a24d51a1c2c
Hitesh <imhr1727@gmail.com>
2023-05-26 00:04:33 +0300

    Jitpack code added.

--- app/src/main/java/com/hr/dialogutils/MainActivity.kt
+++ app/src/main/java/com/hr/dialogutils/MainActivity.kt
@@ -2,18 +2,17 @@ package com.hr.dialogutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hr.custom_dialog.DialogOnClick
-import com.hr.custom_dialog.showMessageDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showMessageDialog(this,"Demmo title","demo message",false,object: DialogOnClick {
+
+       /* showMessageDialog(this,"Demmo title","demo message",false,object: DialogOnClick {
             override fun onClose() {
 
             }
-        })
+        })*/
     }
 }
